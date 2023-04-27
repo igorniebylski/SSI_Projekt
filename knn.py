d18b72a68cd27d48c7e812687843aa305035c6c6
Igor <73477532+igorniebylski4D@users.noreply.github.com>
2023-04-27 12:29:14 +0200

    Update knn.py

--- knn.py
+++ knn.py
@@ -6,6 +6,7 @@ from sklearn.metrics import confusion_matrix, accuracy_score
 import pandas as pd
 import random as r
 import time
+import math
 
 dataset = pd.read_csv("C:\\Users\\igorn\\Desktop\\projekt\\numbers_smaller.csv").copy()
 X=dataset.drop('label', axis=1)
@@ -13,7 +14,7 @@ def KNN():
     accuracy = []
     timer = []
     y=dataset.label
-    for i in range(10):
+    for i in range(100):
         time_start = time.time()
         n = r.randint(1, 15900)
         x_train, x_test, y_train, y_test = train_test_split(X, y, test_size=0.30, random_state=n)
@@ -29,7 +30,17 @@ def KNN():
         accuracy.append(accuracy_score(y_test,y_pred)*100)
         confusion_mat = confusion_matrix(y_test,y_pred)
         timer.append(time.time() - time_start)
-    print("mean accuracy", str(sum(accuracy)/len(accuracy)) + "\nmean time: " + str(sum(timer) / len(timer)))
-
+    avg_acc = sum(accuracy)/len(accuracy)
+    avg_time = sum(timer)/len(timer)
+    dev_acc=0
+    dev_time=0
+    for acc in accuracy:
+        dev_acc+=(acc-avg_acc)**2
+    for t in timer:
+        dev_time+=(float(t)-avg_time)**2
+        
+    dev_acc=math.sqrt(dev_acc/2)
+    dev_time=math.sqrt(dev_time/2)
+    print(avg_acc, dev_acc, avg_time, dev_time)
 
 KNN()
